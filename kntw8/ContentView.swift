41b2f26c106e4a4f5a7db9cd066a8eaff1034b58
sandy <sandrods28@gmail.com>
2021-04-29 23:35:06 -0500

    Initial Commit

---
+++ kntw8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kntw8
+//
+//  Created by user192663 on 4/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kntw8/kntw8App.swift
@@ -0,0 +1,17 @@
+//
+//  kntw8App.swift
+//  kntw8
+//
+//  Created by user192663 on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct kntw8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
